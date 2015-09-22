c50948f320eb083d39302242c65dff24b8fe8540
NiBrossault <ni.brossault@laposte.net>
2015-09-22 14:32:05 +0200

    effacer guillemet

--- app/views/faq/vList.php
+++ app/views/faq/vList.php
@@ -6,12 +6,12 @@ $baseHref=get_class($this); ?>
 	<table class='table table-striped'>
 		<thead><tr><th>Questions</th></tr></thead>
 		<tbody>
-					<?php foreach ($faqs as $faq): ?>
+				<?php foreach ($faqs as $faq): ?>
 				<tr>
 				<td><?php echo $faq->getTitre(); ?></td>
 				<?php if($faq->getUser()->getId() == Auth::getUser()->getId()): ?>
 					<td class='td-center'><a class='btn btn-primary btn-xs' href='<?php echo $baseHref."/frm/".$faq->getId(); ?>'><span class='glyphicon glyphicon-edit' aria-hidden='true'></span></a></td>
-						"<td class='td-center'><a class='btn btn-warning btn-xs' href='<?php echo $baseHref."/delete/".$faq->getId(); ?>'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></a></td>
+						<td class='td-center'><a class='btn btn-warning btn-xs' href='<?php echo $baseHref."/delete/".$faq->getId(); ?>'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></a></td>
 					</tr>
 				<?php else: ?>
 					<td class='td-center'><a class='btn btn-primary btn-xs' href='<?php echo $baseHref."/frm/".$faq->getId(); ?>'><span class='glyphicon glyphicon-eye-open' aria-hidden='true'></span></a></td>
