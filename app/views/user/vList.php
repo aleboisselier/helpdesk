43ed4fe7530be88d3fb6528f9d81702802689996
NiBrossault <ni.brossault@laposte.net>
2015-10-05 10:36:00 +0200

    utilisateur

--- app/views/user/vList.php
+++ app/views/user/vList.php
@@ -1,8 +1,11 @@
+<?php $baseHref=get_class($this); ?>
 <table class='table table-striped'>
 	<tbody>
 		<?php foreach ($users as $user): ?>
 			<tr>
-				<td><?php echo $user; ?></td>
+				<td> <?=$user; ?> </td>
+				<td class='td-center'><a class='btn btn-primary btn-xs' href='<?php echo $baseHref."/frm/".$user->getId() ?>'><span class='glyphicon glyphicon-edit' aria-hidden='true'></span></a></td>
+				<td class='td-center'><a class='btn btn-warning btn-xs' href='<?php echo $baseHref."/delete/".$user->getId() ?>'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></a></td>
 			</tr>
 		<?php endforeach; ?>
 	</tbody>
