67b3a27eeb4d5e68acd34862e4e9ae719eb64d94
Antoine LEBOISSELIER <contact@aleboisselier.fr>
2015-10-19 16:25:30 +0200

    Enhanced Phase 1 #3
    - Added messages when lists are empty

--- app/views/faq/vList.php
+++ app/views/faq/vList.php
@@ -1,31 +1,36 @@
 <div class="col-md-8 faq-items">
-<?php $baseHref=get_class($this); ?>
-	<table class='table table-striped'>
-		<thead>
-			<tr>
-				<?php if(Auth::isAdmin()): ?>
-					<th colspan="3"><h3>Questions</h3></th>
-					<th><a class='btn btn-primary pull-right' href='<?php echo $config["siteUrl"].$baseHref."/frm"; ?>' style="margin-left:2%;" id="faqAddBtn">Ajouter...</a></th>
-				<?php else: ?>
-					<th colspan="4"><h3>Questions</h3></th>
-				<?php endif; ?>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($faqs as $faq): ?>
-				<tr class="faq-item" id="<?php echo $faq->getId(); ?>">
-					<td class="titreFaq" ><?php echo $faq->getTitre(); ?></td>
-					<td class='td-center voirFaq'><a class='btn btn-primary btn-xs btnVoirFaq' href='<?php echo $baseHref."/view/".$faq->getId(); ?>' data-toggle="tooltip" data-placement="top" title="Lire l'Article"><span class='glyphicon glyphicon-edit' aria-hidden='true'></span></a></td>
-					<?php if($faq->getUser()->getId() == Auth::getUser()->getId()): ?>
-						<td class='td-center'><a class='btn btn-danger btn-xs' href='<?php echo $baseHref."/delete/".$faq->getId(); ?>' id="del-<?php echo $faq->getId(); ?>" data-toggle="tooltip" data-placement="top" title="Supprimer l'Article"><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></a></td>
-						<?php if($faq->getPublished()): ?>
-							<td class='td-center publish'><a class='btn btn-warning btn-xs suspend' id="<?php echo $faq->getId(); ?>;0" data-toggle="tooltip" data-placement="top" title="Suspendre l'Article"><span class='glyphicon glyphicon-ban-circle' aria-hidden='true'></span></a></td>
-						<?php else: ?>
-							<td class='td-center publish'><a class='btn btn-success btn-xs suspend' id="<?php echo $faq->getId(); ?>;1" data-toggle="tooltip" data-placement="top" title="Publier l'Article"><span class='glyphicon glyphicon-check' aria-hidden='true'></span></a></td>
-						<?php endif; ?>
-					<?php endif;?>
+	<?php $baseHref=get_class($this); 
+		if(count($faqs) == 0):
+	?>
+		<p class="text-info">Aucun article ne correspond à votre recherche...</p>
+	<?php else: ?>
+		<table class='table table-striped'>
+			<thead>
+				<tr>
+					<?php if(Auth::isAdmin()): ?>
+						<th colspan="3"><h3>Questions</h3></th>
+						<th><a class='btn btn-primary pull-right' href='<?php echo $config["siteUrl"].$baseHref."/frm"; ?>' style="margin-left:2%;" id="faqAddBtn">Ajouter...</a></th>
+					<?php else: ?>
+						<th colspan="4"><h3>Questions</h3></th>
+					<?php endif; ?>
 				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
+			</thead>
+			<tbody>
+				<?php foreach ($faqs as $faq): ?>
+					<tr class="faq-item" id="<?php echo $faq->getId(); ?>">
+						<td class="titreFaq" ><?php echo $faq->getTitre(); ?></td>
+						<td class='td-center voirFaq'><a class='btn btn-primary btn-xs btnVoirFaq' href='<?php echo $baseHref."/view/".$faq->getId(); ?>' data-toggle="tooltip" data-placement="top" title="Lire l'Article"><span class='glyphicon glyphicon-edit' aria-hidden='true'></span></a></td>
+						<?php if($faq->getUser()->getId() == Auth::getUser()->getId()): ?>
+							<td class='td-center'><a class='btn btn-danger btn-xs' href='<?php echo $baseHref."/delete/".$faq->getId(); ?>' id="del-<?php echo $faq->getId(); ?>" data-toggle="tooltip" data-placement="top" title="Supprimer l'Article"><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></a></td>
+							<?php if($faq->getPublished()): ?>
+								<td class='td-center publish'><a class='btn btn-warning btn-xs suspend' id="<?php echo $faq->getId(); ?>;0" data-toggle="tooltip" data-placement="top" title="Suspendre l'Article"><span class='glyphicon glyphicon-ban-circle' aria-hidden='true'></span></a></td>
+							<?php else: ?>
+								<td class='td-center publish'><a class='btn btn-success btn-xs suspend' id="<?php echo $faq->getId(); ?>;1" data-toggle="tooltip" data-placement="top" title="Publier l'Article"><span class='glyphicon glyphicon-check' aria-hidden='true'></span></a></td>
+							<?php endif; ?>
+						<?php endif;?>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+	<?php endif; ?>
 </div>
--- app/views/ticket/vList.php
+++ app/views/ticket/vList.php
@@ -1,75 +1,78 @@
 <div id="list">
 	<br>
-	<?php 
-		foreach ($tickets as $ticket): 
-			$date = date_create($ticket->getDateCreation());
-			$tPerPage = $_SESSION['nbPerPage'];
-			if ($ticket->getType() == "demande") {
-				$demande = 1;
-			}else{
-				$demande = 0;
-			}
-	?>
-			<div class="panel panel-<?php if($demande): ?>info<?php else: ?>warning<?php endif; ?>" style="font-size:110%;">
-				<div class="panel-heading">
-					<?php if($ticket->getStatut()->getId() == 1 && Auth::isAdmin()) : ?>
-							<span class="label label-primary"><span class="glyphicon glyphicon-alert"></span> Nouveau</span>
-					<?php endif; ?>
-					<b> <?= $ticket->getTitre();?></b> - <?= $ticket->getStatut();?>
-					<?php if($demande): ?>
-						<span class="label label-info pull-right">Demande</span>
-					<?php else: ?>
-						<span class="label label-warning pull-right">Incident</span>
-					<?php endif; ?>
-				</div>
-				<div class="panel-body">
-					<div class="col-md-12" >
+	<?php if (count($tickets) == 0):?>
+			<p class="text-info">Aucun ticket ne correspond à votre recherche...</p>
+	<?php else: ?>
+		<?php 
+			foreach ($tickets as $ticket): 
+				$date = date_create($ticket->getDateCreation());
+				$tPerPage = $_SESSION['nbPerPage'];
+				if ($ticket->getType() == "demande") {
+					$demande = 1;
+				}else{
+					$demande = 0;
+				}
+		?>
+				<div class="panel panel-<?php if($demande): ?>info<?php else: ?>warning<?php endif; ?>" style="font-size:110%;">
+					<div class="panel-heading">
+						<?php if($ticket->getStatut()->getId() == 1 && Auth::isAdmin()) : ?>
+								<span class="label label-primary"><span class="glyphicon glyphicon-alert"></span> Nouveau</span>
+						<?php endif; ?>
+						<b> <?= $ticket->getTitre();?></b> - <?= $ticket->getStatut();?>
 						<?php if($demande): ?>
-							Demande de <label> <?= $ticket->getCategorie(); ?></label>, effectuée le  <label> <?= $date->format('d.m.Y');?> </label> à <label> <?= $date->format('H:i');?></label>, par <label> <?= $ticket->getUser()->getLogin(); ?></label>.
+							<span class="label label-info pull-right">Demande</span>
 						<?php else: ?>
-							Problème de <label> <?= $ticket->getCategorie(); ?></label>, signalé le  <label> <?= $date->format('d.m.Y');?> </label> à <label> <?= $date->format('H:i');?></label>, par <label> <?= $ticket->getUser()->getLogin(); ?></label>.
-						<?php endif; ?>
-						<?php if($ticket->getStatut()->getId() > 1): ?> 
-							<br>Attribué à : <label><?= $ticket->getAdmin()->getLogin(); ?></label>.
+							<span class="label label-warning pull-right">Incident</span>
 						<?php endif; ?>
 					</div>
-					<div class="clearfix" style="margin-top:2%">&nbsp;</div>
-					<?php if(Auth::isAdmin()): ?>
-						<div class="col-md-8" >
-							<div class="btn-group" role="group">
-								<?php error_reporting(0);echo Tickets::getButtonGroup($ticket);error_reporting(-1);?>
-							</div>
+					<div class="panel-body">
+						<div class="col-md-12" >
+							<?php if($demande): ?>
+								Demande de <label> <?= $ticket->getCategorie(); ?></label>, effectuée le  <label> <?= $date->format('d.m.Y');?> </label> à <label> <?= $date->format('H:i');?></label>, par <label> <?= $ticket->getUser()->getLogin(); ?></label>.
+							<?php else: ?>
+								Problème de <label> <?= $ticket->getCategorie(); ?></label>, signalé le  <label> <?= $date->format('d.m.Y');?> </label> à <label> <?= $date->format('H:i');?></label>, par <label> <?= $ticket->getUser()->getLogin(); ?></label>.
+							<?php endif; ?>
+							<?php if($ticket->getStatut()->getId() > 1): ?> 
+								<br>Attribué à : <label><?= $ticket->getAdmin()->getLogin(); ?></label>.
+							<?php endif; ?>
 						</div>
-					<?php endif; ?>
-					<div class="col-md-4 pull-right">
-						<a href="tickets/frm/<?= $ticket->getId(); ?>" class="btn btn-default pull-right detailTicket"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Afficher les détails</a>
+						<div class="clearfix" style="margin-top:2%">&nbsp;</div>
+						<?php if(Auth::isAdmin()): ?>
+							<div class="col-md-8" >
+								<div class="btn-group" role="group">
+									<?php error_reporting(0);echo Tickets::getButtonGroup($ticket);error_reporting(-1);?>
+								</div>
+							</div>
+						<?php endif; ?>
+						<div class="col-md-4 pull-right">
+							<a href="tickets/frm/<?= $ticket->getId(); ?>" class="btn btn-default pull-right detailTicket"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Afficher les détails</a>
+						</div>
 					</div>
 				</div>
-			</div>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
 
-	<?php 
-		$nbPages = ceil($nbTickets/$tPerPage);
-	?>
+		<?php 
+			$nbPages = ceil($nbTickets/$tPerPage);
+		?>
 
-	<div class="text-center" id="pagination">
-		<nav>
-			<ul class="pagination">
-				<li <?php if($currPage == 1) :?> class="disabled" <?php else:?> class="chgList" id="<?php echo ($currPage-1).';'.$tPerPage; ?>" <?php endif; ?>>
-					<a aria-label="Précédent">
-						<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
-					</a>
-				</li>
-					<?php for($i = 1; $i<=$nbPages; $i++): ?>
-						<li <?php if($i == $currPage):?>class="active"<?php endif;?> id="<?php echo $i.';'.$tPerPage; ?>" class="chgList"><a><?php echo $i; ?></a></li>
-					<?php endfor; ?>
-				<li <?php if($currPage == $nbPages) :?> class="disabled" <?php else:?> class="chgList" id="<?php echo ($currPage+1).';'.$tPerPage; ?>" <?php endif; ?>>
-					<a aria-label="Suivant" >
-						<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-					</a>
-				</li>
-			</ul>
-		</nav>
+		<div class="text-center" id="pagination">
+			<nav>
+				<ul class="pagination">
+					<li <?php if($currPage == 1) :?> class="disabled" <?php else:?> class="chgList" id="<?php echo ($currPage-1).';'.$tPerPage; ?>" <?php endif; ?>>
+						<a aria-label="Précédent">
+							<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+						</a>
+					</li>
+						<?php for($i = 1; $i<=$nbPages; $i++): ?>
+							<li <?php if($i == $currPage):?>class="active"<?php endif;?> id="<?php echo $i.';'.$tPerPage; ?>" class="chgList"><a><?php echo $i; ?></a></li>
+						<?php endfor; ?>
+					<li <?php if($currPage == $nbPages) :?> class="disabled" <?php else:?> class="chgList" id="<?php echo ($currPage+1).';'.$tPerPage; ?>" <?php endif; ?>>
+						<a aria-label="Suivant" >
+							<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</div>
-</div>
-
+<?php endif; ?>
