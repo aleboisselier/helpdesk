7807ef936d39d846a137e56342a87e16c951f6d5
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2015-10-28 18:12:32 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- app/config.php
+++ app/config.php
@@ -1,22 +1,22 @@
 <?php
 return array(
-		"siteUrl"=>"http://127.0.0.1/helpdesk/",
-		"documentRoot"=>"Indexx",
-		"database"=>[
-				"dbName"=>"helpdesk",
-				"serverName"=>"127.0.0.1",
-				"port"=>"3306",
-				"user"=>"root",
-				"password"=>""
-		],
-		"directories"=>["my","tests"],
-		"test"=>false,
-		"mails"=>[
-				"smtp"=> "smtp.gmail.com",
-				"smtpAuth" => true,
-				"username"=> "gmailID@gmail.com",
-				"password"=> "GmailPassword",
-				"port" => 587,
-				"secure"=>"tls"
-		]
+        "siteUrl"=>"http://127.0.0.1/helpdesk/",
+        "documentRoot"=>"Indexx",
+        "database"=>[
+                "dbName"=>"helpdesk",
+                "serverName"=>"127.0.0.1",
+                "port"=>"3306",
+                "user"=>"root",
+                "password"=>""
+        ],
+        "directories"=>["my","tests"],
+        "test"=>false,
+        "mails"=>[
+                "smtp"=> "smtp.gmail.com",
+                "smtpAuth" => true,
+                "username"=> "gmailID@gmail.com",
+                "password"=> "GmailPassword",
+                "port" => 587,
+                "secure"=>"tls"
+        ]
 );
