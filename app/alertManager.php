6e19d21017d9f16714aa50215bbe77b6898750b4
Antoine LEBOISSELIER <contact@aleboisselier.fr>
2015-11-09 01:37:48 +0100

    Trying to concatenate mail alerts
    Not very concluant, I tried

--- app/alertManager.php
+++ app/alertManager.php
@@ -24,47 +24,59 @@ while (true) {
 	$notifs = DAO::getAll("Notification", "mailSent = 0");
 	foreach ($notifs as $notif){
 		$alert = DAO::getOne("Alert", "idUser=".$notif->getUser()->getId());
-		$freq = json_decode($alert->getFrequence(), true);
 		
-		//Converting my days to PHP days values
-		$freqC = array();
-		foreach ($freq as $f){
-			if(intval($f['day']) == 6){
-				array_push($freqC, array("day"=>0, "time"=>$f["time"]));
-			}else{
-				array_push($freqC, array("day"=>intval($f["day"])+1, "time"=>$f["time"]));
+		if($alert->getEnabled()){
+			
+			$freq = json_decode($alert->getFrequence(), true);
+			
+			//Converting my days to PHP days values
+			$freqC = array();
+			foreach ($freq as $f){
+				if(intval($f['day']) == 6){
+					array_push($freqC, array("day"=>0, "time"=>$f["time"]));
+				}else{
+					array_push($freqC, array("day"=>intval($f["day"])+1, "time"=>$f["time"]));
+				}
 			}
-		}
-		
-		$today = getdate()["wday"];
-		foreach ($freqC as $f){
-			if (($today == $f['day'] && time() >= strtotime($f["time"]) )|| $alert->getInstant()){
-					require_once ROOT.'../vendor\phpmailer\phpmailer/PHPMailerAutoload.php';
-					$mail = new PHPMailer;
-					$mail->isSMTP();
-					$mail->SMTPDebug = 0;
 			
-					$mail->Host = $config['mails']['smtp'];
-					$mail->Port = $config['mails']['port'];
+			$mails = array();
 			
-					$mail->SMTPSecure = $config['mails']['secure'];
-					$mail->SMTPAuth = $config['mails']['smtpAuth'];
-					$mail->Username = $config['mails']['username'];
-					$mail->Password = $config['mails']['password'];
-			
-					$mail->setFrom('no-reply@helpdesk.com', 'Centre de Notifications HelpDesk');
-					$mail->addAddress($to);
-					$mail->Subject = 'Nouvelle(s) Notification(s)';
-					$mail->CharSet = 'UTF-8';
-					$mail->msgHTML('<a href="'.$config['siteUrl'].'Tickets/frm/'.$notif->getTicket()->getId().'#'.$notif->getMessage()->getId().'" class="list-group-item notif">'.$notif.'</a>', dirname(__FILE__));
-			
-					$mail->send();
-					
-					$notif->setMailSent(1);
-					DAO::update($notif);
+			$today = getdate()["wday"];
+			foreach ($freqC as $f){
+				if (($today == $f['day'] && time() >= strtotime($f["time"]) )|| $alert->getInstant()){
+					$message = '<a href="'.$config['siteUrl'].'Tickets/frm/'.$notif->getTicket()->getId().'#'.$notif->getMessage()->getId().'" class="list-group-item notif">'.$notif.'</a>';
+					if (!array_key_exists($notif->getUser()->getId(), $mails))
+						array_push($mails, $notif->getUser()->getId());
+					array_push($mails[$notif->getUser()->getId()], $message);
+				}
 			}
+			print_r($mails);
 		}
 	}
 	
 	sleep(10);
 }
+/*
+require_once ROOT.'../vendor\phpmailer\phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 0;
+	
+$mail->Host = $config['mails']['smtp'];
+$mail->Port = $config['mails']['port'];
+	
+$mail->SMTPSecure = $config['mails']['secure'];
+$mail->SMTPAuth = $config['mails']['smtpAuth'];
+$mail->Username = $config['mails']['username'];
+$mail->Password = $config['mails']['password'];
+	
+$mail->setFrom('no-reply@helpdesk.com', 'Centre de Notifications HelpDesk');
+$mail->addAddress($to);
+$mail->Subject = 'Nouvelle(s) Notification(s)';
+$mail->CharSet = 'UTF-8';
+$mail->msgHTML($message, dirname(__FILE__));
+	
+$mail->send();
+	
+$notif->setMailSent(1);
+DAO::update($notif);
